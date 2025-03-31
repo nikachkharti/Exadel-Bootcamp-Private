e35870f9bb1b80935aaacabbf59b6c21f31b9be0
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-31 17:42:51 +0400

    Identity server project initialize

---
+++ Bootcamp/IdentityServer/Extensions/ContainerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace IdentityServer.Extensions
+{
+    public static class ContainerExtensions
+    {
+        public static void AddControllers(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+
+        public static void AddSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                #region XML documentation
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+                #endregion
+            });
+        }
+
+    }
+}
---
+++ Bootcamp/IdentityServer/Program.cs
@@ -0,0 +1,26 @@
+using IdentityServer.Extensions;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.AddControllers();
+            //builder.Services.AddOpenApi();
+            builder.AddSwagger();
+
+            var app = builder.Build();
+
+            //app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
