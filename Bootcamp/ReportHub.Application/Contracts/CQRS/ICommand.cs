5811c86ca103d9325e93a2214b88400a2d130909
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-31 09:41:27 +0400

    Base contacts for working with CQRS and MediatR

---
+++ Bootcamp/ReportHub.Application/Contracts/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ReportHub.Application.Contracts.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResposne> : IRequest<TResposne>
+    {
+    }
+}
---
+++ Bootcamp/ReportHub.Application/Contracts/CQRS/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ReportHub.Application.Contracts.CQRS
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull
+    {
+    }
+}
