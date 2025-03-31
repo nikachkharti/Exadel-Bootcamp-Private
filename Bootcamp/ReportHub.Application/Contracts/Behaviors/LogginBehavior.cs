4d233c1abf43b32725108f6935822bcabb1108f7
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-31 09:56:30 +0400

    Generic logging and validation behaviors

---
+++ Bootcamp/ReportHub.Application/Contracts/Behaviors/LogginBehavior.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ReportHub.Application.Contracts.Behaviors
+{
+    public class LogginBehavior<TRequest, TResponse>
+        (ILogger<LogginBehavior<TRequest, TResponse>> logger)
+        :
+        IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("[START] Handle request = {Request} - Response = {Resposne} - RequestData = {RequestData}", typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var respone = await next();
+
+            timer.Stop();
+
+            var timeTaken = timer.Elapsed;
+
+            //If the request is greater than 3 seconds, then log a warning.
+            if (timeTaken.Seconds > 3)
+            {
+                logger.LogWarning("[PERFORMANCE] The request = {Request} took {TimeTaken}", typeof(TRequest).Name, timeTaken);
+            }
+
+            logger.LogInformation("[END] Handled {Request} with {Response}", typeof(TRequest).Name, typeof(TResponse).Name);
+            return respone;
+        }
+    }
+}
---
+++ Bootcamp/ReportHub.Application/Contracts/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using ReportHub.Application.Contracts.CQRS;
+
+namespace ReportHub.Application.Contracts.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>
+        (IEnumerable<IValidator<TRequest>> validators)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validatorResults = await Task
+                .WhenAll(validators
+                .Select(v => v
+                .ValidateAsync(context, cancellationToken)));
+
+            var failures =
+                validatorResults
+                .Where(r => r.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+
+            return await next();
+        }
+    }
+}
